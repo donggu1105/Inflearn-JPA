b9fdcddd2c71806155a251926f7e556c3dc4e209
donggu1105 <kbh11051@gmail.com>
2020-12-22 16:01:38 +0900

    h2 database 연결

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,16 +1,32 @@
 package hellojpa;
 
-import org.hibernate.internal.build.AllowSysOut;
-
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-       EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-       System.out.println("123");
-        System.out.println("test");
+        EntityManager em = emf.createEntityManager();/*JPA에서는 항상 트랜잭션 관리를 해줘야*/
+
+        EntityTransaction tr = em.getTransaction();
+        tr.begin();
+
+        //code
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("name");
+        em.persist(member);
+
+        tr.commit();
+
+
+        em.close();
+        emf.close();
     }
 }
+
+
