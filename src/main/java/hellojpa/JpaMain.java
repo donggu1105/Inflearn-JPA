1c01130814293c4dccfea7155c5dd79de94e07ff
donggu1105 <kbh11051@gmail.com>
2020-12-22 11:48:22 +0900

    인텔리제이 커밋

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,16 @@
+package hellojpa;
+
+import org.hibernate.internal.build.AllowSysOut;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+       EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+       System.out.println("123");
+
+    }
+}
