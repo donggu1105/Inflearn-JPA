00faf72d9fd75777e72e1d0a5e10ccda2745d201
donggu1105 <kbh11051@gmail.com>
2020-12-23 10:08:50 +0900

    Hello JPA 프로젝트 완성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,8 +18,13 @@ public class JpaMain {
 
         //code
 
-        try {
-            List<Member> result = em.createQuery("SELECT m FROM Member as m").getResultList();
+        try {/*데이터베이스 테이블을 대상으로 하는게 아닌 JPQL 객체대상으로 동적쿼리를 짤수있도록 하는것*/
+            /*테이블이 아닌 객체를 대상으로 검색하는 객체 지향쿼리
+            * */
+            List<Member> result = em.createQuery("SELECT m FROM Member as m WHERE 1=1")
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
 
             for(Member m : result) {
                 System.out.println("Member :"+ m.getName());
