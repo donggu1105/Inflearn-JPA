06bff6d0e0ae5b52160f9cb66cccfec68e857e30
donggu1105 <kbh11051@gmail.com>
2020-12-23 09:56:46 +0900

    JPA 수정

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -18,7 +19,11 @@ public class JpaMain {
         //code
 
         try {
+            List<Member> result = em.createQuery("SELECT m FROM Member as m").getResultList();
 
+            for(Member m : result) {
+                System.out.println("Member :"+ m.getName());
+            }
             tr.commit();
         } catch (Exception e) {
             tr.rollback();
